a797851c3939fb2e3d0373d7b1e2a02eb280b049
Shadman Alam <shadman.alam@servian.com>
2017-12-10 22:23:01 +1100

    updated notebook

--- data_provider.py
+++ data_provider.py
@@ -20,6 +20,7 @@
  MA 02110-1301, USA.
 
 Run this generatate new training data. 
+Note: The api is limited to 10 requests per minute, therefore we have a delay of 6.5 seconds between each call
 """
 
 
@@ -37,8 +38,8 @@ import pandas as pd
 
 START_DATA = 1
 END_DATE = 32
+YEARMONTH = ''
 DELAY = 6.5
-YEARMONTH = '201710'
 COUNTRY = 'Australia'
 KEY = '15f7f977b8b6a26a'
 
@@ -76,8 +77,7 @@ def request(url):
 def clean(raw_data):
     clean_data = []
     for obj in raw_data['history']['observations']:
-        tmp = {key: val for key, val in obj.items() if key in features_list and obj[
-            key] not in ('', '-999', '-9999', '-9999.0', '-9999.00')}
+        tmp = {key: val for key, val in obj.items() if key in features_list and obj[key] not in ('', '-999', '-9999', '-9999.0', '-9999.00')}
         clean_data.append(tmp)
     hourly_features = {each['date']['hour']: each for each in clean_data}
     return hourly_features
@@ -86,7 +86,8 @@ def clean(raw_data):
 def write_to_file(data, date, city):
     current_directory = os.getcwd()
     resource_directory = os.path.join(current_directory, 'resource')
-    final_directory = os.path.join(resource_directory, city)
+    date_directory = os.path.join(resource_directory, YEARMONTH)
+    final_directory = os.path.join(date_directory, city)
     if not os.path.exists(final_directory):
         os.makedirs(final_directory)
     file_name = date+'.txt'
@@ -109,8 +110,8 @@ def extract():
             url = "http://api.wunderground.com/api/{key}/history_{date}/q/{country}/{city}.json".format(
                 **param)
             sleep(DELAY)
-            print(url)
-            response = request('@URL: - ' + url)
+            print('@URL: - ' + url)
+            response = request(url)
             json_data = clean(response)
             write_to_file(json_data, param['date'], city)
             print("DONE : -> " + " - city - " +
@@ -120,45 +121,72 @@ def extract():
 def consolidate_to_csv():
     csv_rows = []
     for city in city_list:
-        city_directory = os.path.join('.', city)
-        day_files = [f for f in os.listdir(city_directory) if f[-4:] == '.txt']
+        current_directory = os.getcwd()
+        resource_directory = os.path.join(current_directory, 'resource')
+        date_directory = os.path.join(resource_directory, YEARMONTH)
+        if  os.path.isdir(date_directory+'/'+city):
+            city_directory = os.path.join(date_directory, city)
+            day_files = [f for f in os.listdir(city_directory) if f[-4:] == '.txt']
 
-        # Loop through every day
-        for day_file in day_files:
-            day_file_path = os.path.join(city_directory, day_file)
-            day_data = json.load(open(day_file_path))
+            # Loop through every day
+            for day_file in day_files:
+                day_file_path = os.path.join(city_directory, day_file)
+                day_data = json.load(open(day_file_path))
 
-            # Get the day number from the file name
-            day = day_file.split('.txt')[0][6:]
+                # Get the day number from the file name
+                day = day_file.split('.txt')[0][6:]
 
-            # Loop through every hour:
-            for hour in [key for key in day_data.keys()]:
-                csv_row = [city, day, hour]
-                for feature in features_list[1:]:
-                    feature_data = day_data[hour].get(feature)
-                    csv_row.append(feature_data)
-                csv_rows.append(csv_row)
+                # Loop through every hour in each file:
+                for hour in [key for key in day_data.keys()]:
+                    csv_row = [city, day, hour]
+                    for feature in features_list[1:]:
+                        feature_data = day_data[hour].get(feature)
+                        csv_row.append(feature_data)
+                    csv_rows.append(csv_row)
 
     columns = ['City', 'Day', 'Hour', 'Condition', 'Humidity',
                'Temp', 'Pressure', 'DewPt', 'WindSpeed', 'Visibility']
     all_data = pd.DataFrame(csv_rows, columns=columns)
-    all_data.to_csv('../training_data/input_data_oct2.csv')
+
+    training_directory = os.path.join(current_directory, 'training_data')
+    if not os.path.exists(training_directory):
+        os.makedirs(training_directory)
+    
+    output_file_path = os.path.join(training_directory, 'input_data_{}.csv'.format(YEARMONTH))
+    all_data.to_csv(output_file_path)
 
 
 def main(argv):
     try:
-
-        if len(argv) < 2:
-            print("Enter Sample like - python3 data_provider.py test ")
-            sys.exit()
-        
-        if 'fetch' in argv:
-            extract()
-        if 'compile' in argv:
-            consolidate_to_csv()
         if 'test' in argv:
             import ipdb
             ipdb.set_trace()
+
+        if '--month' in argv and len(argv) > 2:
+            
+            args_map = { args:index for index,args in enumerate(argv) }
+            month = argv[ args_map['--month'] + 1]
+            
+            if int( month[4:]) in range(1,13):
+                global YEARMONTH
+                YEARMONTH = argv[ args_map['--month'] + 1]
+
+            else:
+                raise Exception("Enter Sample like - python3 data_provider.py [ fetch | compile | test ] --month 201702 or check the month!")
+                sys.exit()
+        
+
+            if 'fetch'in argv:
+                print('Extracting for year - ' + YEARMONTH)
+                extract()
+
+            if 'compile' in argv:
+                consolidate_to_csv()
+                print('Consolidated')
+        else:
+            raise Exception("Enter Sample like - python3 data_provider.py [ fetch | compile | test ] --month 201702 or check the month!")
+            sys.exit()
+
             
     except Exception as e:
         print(e)
